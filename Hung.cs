26c46538d89324dc2370bb826e16e24386a9c706
Enzo <enzolabatte@gmail.com>
2023-09-04 15:34:32 -0300

    hung complete v1

--- Hung.cs
+++ Hung.cs
@@ -4,45 +4,6 @@ namespace BlackBox_Proyect_One
     {
         public static void Start()
         {
-            GuessingWord();
-        }
-        public static void Guy(int x, int y, bool rightFeet, bool leftFeet, bool torso, bool righHand, bool leftHand, bool head, int wrong)
-        {
-            //int wrong=0;
-            bool running=true;
-            while (running)
-            {
-                Tls.colorFlip(ConsoleColor.Black,ConsoleColor.DarkRed);
-                Console.SetCursorPosition(x, y);
-                Draw.down(7,'█',null,null);
-                Draw.right(5,'█',null,null);
-                Console.SetCursorPosition(x, y);
-                Draw.right(8,'█',null,null);
-                Draw.down(2,'│','█',null);
-                
-                Console.SetCursorPosition(x, y);
-
-                if (wrong==1){rightFeet=true;}
-                if (wrong==2){leftFeet=true;}
-                if (wrong==3){torso=true;}
-                if (wrong==4){righHand=true;}
-                if (wrong==5){leftHand=true;}
-                if (wrong==6){head=true;running=false;}
-
-                Tls.colorFlip(ConsoleColor.Black,ConsoleColor.Red);
-                
-                if (rightFeet){Draw.point(x+9,y+4,'\\');}
-                if (leftFeet){Draw.point(x+7,y+4,'/');}
-                if (torso){Draw.point(x+8,y+3,'|');}
-                if (righHand){Draw.point(x+9,y+3,'\\');}
-                if (leftHand){Draw.point(x+7,y+3,'/');}
-                if (head){Draw.point(x+8,y+2,'0'); Tls.ParalelOut(x+12,y+5,"You Lost");}
-            }
-        } // end Guy method
-
-        public static void GuessingWord()
-        {
-
             // lista de palabras
             List<string> listaDePalabras = new List<string>
             {
@@ -50,13 +11,20 @@ namespace BlackBox_Proyect_One
                 "elemento",
                 "murcielago",
                 "hospital",
-                "computadora"
+                "computadora",
+                "emoji",
+                "pastor",
+                "dios",
+                "mochila",
+                "auto",
+                "gorila",
+                "argentina",
+                "atmosfera",
             };
 
             //le asignamos a la palabra secreta la palabra random que obtuvo el metodo ChooseWord
             string secretWord = ChooseWord(listaDePalabras);
             string hiddenWord = new string('_', secretWord.Length);
-            Console.WriteLine(hiddenWord);
 
             //variables para saber si estamos jugando y "cuantas vidas tenemos" 
             bool gameGoing = true;
@@ -64,17 +32,22 @@ namespace BlackBox_Proyect_One
 
             while(gameGoing)
             {
-                bool rightFeet= false, leftFeet= false, torso= false, righHand= false, leftHand= false, head = false;
-                int x=0,y=0;
-                Guy(x,y,rightFeet, leftFeet, torso, righHand, leftHand, head, wrongAttempts);
+                 int x=0,y=0;
 
-                Console.Clear();
-                Console.SetCursorPosition(1, 20);
-                Console.WriteLine("guess the word: " + hiddenWord);
-                Console.WriteLine($"Wrong Attemps: {wrongAttempts}");
+                //dibujo de la horca
+                Tls.colorFlip(ConsoleColor.Black,ConsoleColor.DarkRed);
+                Console.SetCursorPosition(x, y);
+                Draw.down(7,'█',null,null);
+                Draw.right(5,'█',null,null);
+                Console.SetCursorPosition(x, y);
+                Draw.right(8,'█',null,null);
+                Draw.down(2,'│','█',null);
 
-                // Leer la letra ingresada por el jugador
-                Console.Write("Write a Letter: ");
+                //palabra oculta que vamos a adivinar y los intentos que nos quedan
+                Tls.ParalelOut(x+15,y+8,$"Guess the Word: {hiddenWord}");
+                Tls.ParalelOut(x+15,y+10,$"Attempts Left: {6 - wrongAttempts}");
+
+                // leer la letra ingresada por el jugador
                 char letra = Console.ReadKey().KeyChar;
 
                 if (secretWord.Contains(letra))
@@ -86,39 +59,59 @@ namespace BlackBox_Proyect_One
                         {
                             hiddenWord = hiddenWord.Remove(i, 1).Insert(i, letra.ToString());
                         }
+                        
                     }
-
                     // verificar si adivinó la palabra
                     if (hiddenWord == secretWord)
                     {
                         gameGoing = false;
                         Console.Clear();
                         Console.WriteLine("¡YOU WON!");
-                        Console.WriteLine("Word: " + secretWord);
-                    }
+                    }  
+                }
 
-                    else
+                else
+                {
+                    //incrementa los wrongAttempts hasta 6 (la cantidad de figuras que entran en el ahorcado)
+                    wrongAttempts++;
+                    bool rightFeet = false, leftFeet = false, torso = false, righHand = false, leftHand = false, head= false;
+
+                    Console.SetCursorPosition(x, y);
+
+                    //cada vez que haces un intento erroneo dibuja una parte del cuerpo
+                    if (wrongAttempts==1){rightFeet=true;}
+                    if (wrongAttempts==2){leftFeet=true;}
+                    if (wrongAttempts==3){torso=true;}
+                    if (wrongAttempts==4){righHand=true;}
+                    if (wrongAttempts==5){leftHand=true;}
+                    if (wrongAttempts==6){head=true;}
+
+                    Tls.colorFlip(ConsoleColor.Black,ConsoleColor.Red);
+                    
+                    //dibujo del hombre
+                    if (rightFeet == true){Draw.point(x+9,y+4,'\\');}
+                    if (leftFeet){Draw.point(x+7,y+4,'/');}
+                    if (torso){Draw.point(x+8,y+3,'|');}
+                    if (righHand){Draw.point(x+9,y+3,'\\');}
+                    if (leftHand){Draw.point(x+7,y+3,'/');}
+                    if (head)
                     {
-                        wrongAttempts++;
-                        if(wrongAttempts == 6)
-                        {
-                            //Console.WriteLine("¡YOU LOST!");
-                            Console.WriteLine("The Word Is: " + secretWord);
-                            gameGoing = false;
-                        }
+                        Draw.point(x+8,y+2,'0');
+                        gameGoing = false;
+                        Console.Clear();
+                        Tls.ParalelOut(y+12,x+5,"You Lost");
+                        Tls.ParalelOut(y+12,x+7,"Word: " + secretWord);
                     }
                 }
             }
-
         }// end GuessingWord method
 
+        //metodo para elegir alguna palabra de la lista
         static string ChooseWord(List<string> listaDePalabras)
         {
             Random random = new Random();
             int indiceAleatorio = random.Next(0, listaDePalabras.Count);
             return listaDePalabras[indiceAleatorio];
         }
-
-    }//end hung
-
+    }//end hung   
 }//end workspace
